1798b0f2050c42c013e78394de8b90a30389cab8
superkkj <gabunot@gmail.com>
2022-04-07 16:49:01 +0900

    featrue: 리디스 설정 (미완)

--- src/main/java/bebeShare/config/redis/RedisConfig.java
+++ src/main/java/bebeShare/config/redis/RedisConfig.java
@@ -1,7 +1,11 @@
 package bebeShare.config.redis;
 
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -21,9 +25,33 @@ public class RedisConfig extends AbstractHttpSessionApplicationInitializer {
     private Integer port;
 
 
+//    @Bean
+//    public LettuceConnectionFactory connectionFactory() {
+//        return new LettuceConnectionFactory(host, port);
+//
+//
+//    }
+
     @Bean
-    public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory(host, port); // ip, port
+    public LettuceClientConfigurationBuilderCustomizer lettuceClientConfigurationBuilderCustomizer() {
+        return clientConfigurationBuilder -> {
+            if (clientConfigurationBuilder.build().isUseSsl()) {
+                clientConfigurationBuilder.useSsl().disablePeerVerification();
+            }
+        };
     }
+
+    public static StatefulRedisConnection<String, String> connect() {
+        System.out.println("redis!!");
+        RedisURI redisURI = RedisURI.create(System.getenv("redis: // : pae39b99f39db2c0e45ff88155be2bfd0445aa604c574ce084b84baf3b12e436d @ ec2-18-205-76-248.compute-1.amazonaws.com :"));
+        redisURI.setVerifyPeer(false);
+
+        RedisClient redisClient = RedisClient.create(redisURI);
+        return redisClient.connect();
+    }
+
+
+
+
 
 }

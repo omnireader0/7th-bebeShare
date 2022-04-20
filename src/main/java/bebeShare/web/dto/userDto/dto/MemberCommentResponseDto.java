3a6939169ba42f7d7aa1f89974801ba6fb1ddeaa
superkkj <gabunot@gmail.com>
2022-04-21 01:41:24 +0900

    featrue: 디렉토리분리

---
+++ src/main/java/bebeShare/web/dto/userDto/dto/MemberCommentResponseDto.java
@@ -0,0 +1,29 @@
+package bebeShare.web.dto.userDto.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+public class MemberCommentResponseDto {
+
+    private Long commentId;
+    private String productName;
+    private String commentContent;
+    private LocalDateTime insertDt;
+
+
+
+    @QueryProjection //의존적이게 되 단점
+    public MemberCommentResponseDto(Long commentId, String productName, String commentContent, LocalDateTime insertDt) {
+        this.commentId = commentId;
+        this.productName   = productName;
+        this.commentContent = commentContent;
+        this.insertDt = insertDt;
+
+    }
+
+}

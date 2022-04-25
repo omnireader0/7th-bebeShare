f3ec98e350f47dc1e16e8a4fefe86719116533b2
superkkj <gabunot@gmail.com>
2022-04-25 14:08:32 +0900

    featrue: 수정 기능 오류 해결

--- src/main/java/bebeShare/web/dto/productDto/ProductCreateRequestDto.java
+++ src/main/java/bebeShare/web/dto/productDto/ProductCreateRequestDto.java
@@ -11,9 +11,6 @@ public class ProductCreateRequestDto {
     private User user;
     private String productName;
     private String productContent;
-    private String productImage1;
-    private String productImage2;
-    private String productImage3;
     private String productCategory;
     private String productStatus;
     private String deleteYn;
@@ -23,9 +20,6 @@ public class ProductCreateRequestDto {
         this.user = user;
         this.productName = productName;
         this.productContent = productContent;
-        this.productImage1 = productImage1;
-        this.productImage2 = productImage2;
-        this.productImage3 = productImage3;
         this.productCategory = productCategory;
         this.productStatus = productStatus;
         this.deleteYn = deleteYn;
@@ -38,9 +32,6 @@ public class ProductCreateRequestDto {
                 .productName(productName)
                 .productCategory(productCategory)
                 .productContent(productContent)
-                .productImage1(productImage1)
-                .productImage2(productImage2)
-                .productImage3(productImage3)
                 .productStatus(productStatus)
                 .deleteYn(deleteYn)
                 .build();

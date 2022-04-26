eee2ade992bc1c0780d33dbedfddbe03e80e35d7
TevLee <ltwy0311@naver.com>
2022-04-26 15:52:13 +0900

    feature:이미지 다운로드 수정중

--- src/main/java/bebeShare/service/ImageService.java
+++ src/main/java/bebeShare/service/ImageService.java
@@ -6,6 +6,7 @@ import bebeShare.exception.ErrorCode;
 import com.google.cloud.storage.Bucket;
 import com.google.firebase.cloud.StorageClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -16,11 +17,14 @@ import static java.util.Objects.hash;
 @Slf4j
 @Service
 public class ImageService {
-    public static String createImage(byte[] image, User user){
-        Bucket bucket = StorageClient.getInstance().bucket();
+    @Autowired
+    private Bucket bucket;
+
+    public String createImage(byte[] image, User user){
         //Image 저장 위치를 선언
         String blob = "products/images/"+user.getId()+"/"+hash(image);
-
+        log.info("blob ={}",blob);
+        log.info("bucket ={}",bucket);
         try{
             // 이미 존재하면 파일 삭제
             if(bucket.get(blob) != null){
@@ -33,5 +37,11 @@ public class ImageService {
         }catch(Exception e){
             throw new CustomException(ErrorCode.IMAGE_UPLOAD_FAILED);
         }
+    }
+
+    public byte[] getImage(String userid,String filename){
+        String bucketPath = "products/images/"+userid+"/"+filename;
+        log.info("userid={}",userid);
+        return bucket.get(bucketPath).getContent();
     }
 }
--- src/main/java/bebeShare/web/ImageController.java
+++ src/main/java/bebeShare/web/ImageController.java
@@ -3,10 +3,9 @@ package bebeShare.web;
 import bebeShare.domain.user.User;
 import bebeShare.service.ImageService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
@@ -15,11 +14,22 @@ import java.io.IOException;
 @Controller
 @Slf4j
 public class ImageController {
+    @Autowired
+    private ImageService imageService;
+
     @PostMapping("/upload")
     public @ResponseBody String imageUpload(@RequestParam("image") MultipartFile image, HttpSession session) throws IOException {
         log.info("image={}",image);
-
+        log.info("imageService={}",imageService);
         if(image.isEmpty()) return null;
-        return ImageService.createImage(image.getBytes(), (User)session.getAttribute("user"));
+        return imageService.createImage(image.getBytes(), (User)session.getAttribute("user"));
+    }
+
+
+    @GetMapping("/products/images/{userId}/{fileName}")
+    public byte[] imageDownload(@PathVariable("userId") String userid, @PathVariable("fileName") String filename) {
+        System.out.println("ImageController.imageDownload");
+        log.info("userId={}",userid);
+        return imageService.getImage(userid,filename);
     }
 }
